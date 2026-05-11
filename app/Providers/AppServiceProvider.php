6f8c38d5c5681f24dfd194a4b2b5167e1becd389
AsgharSHAH <asghar786@hotmail.com>
2026-05-11 17:05:52 +0500

    fix: share site settings to all views via View composer
    Blade child sections are captured before the layout @php block runs,
    so $sitePhone/$sitePhoneTel were undefined in @section content.
    Added a wildcard View::composer in AppServiceProvider that shares
    phone, email, and address to every view on every request.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use App\View\Composers\NavigationComposer;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -13,5 +14,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('layouts.app', NavigationComposer::class);
+
+        View::composer('*', function (\Illuminate\View\View $view): void {
+            $sitePhone = Setting::get('phone', '+1 (305) 906-5182');
+            $view->with([
+                'sitePhone'    => $sitePhone,
+                'sitePhoneTel' => 'tel:' . preg_replace('/[^\d+]/', '', $sitePhone),
+                'siteEmail'    => Setting::get('email', 'syeds@scs-technologies.com'),
+                'siteAddress'  => Setting::get('address_miami', '10125 NW 116th Way, Medley, Florida 33178'),
+            ]);
+        });
     }
 }
